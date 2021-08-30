af3ad2783511ac32518af7a2175bcb39d599bc22
iragh <83887242+iragh@users.noreply.github.com>
2021-08-30 16:06:04 -0300

    Update vuln_bootstrap.sh

--- vuln_bootstrap.sh
+++ vuln_bootstrap.sh
@@ -45,7 +45,7 @@ sudo service nginx restart
 sleep 2
 sudo nginx -s reload
 
-# Download the CPnanoAgent - vm Azure CHKP Day 2
+# Download the CPnanoAgent - vm Azure 30ago21v1
 until curl \
     --output /home/$name/cp-nano-egg \
     --url https://sc1.checkpoint.com/nanoegg/cp-nano-egg ; do
